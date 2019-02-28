c399c68d1c25c3155e633e34012479cee3a667f6
Franco Melandri <fmelandri@imolainformatica.it>
2019-02-28 16:50:14 +0100

    fixing string interpolatoin

--- pipeline/docker.groovy
+++ pipeline/docker.groovy
@@ -1,7 +1,7 @@
 def buildDockerImage (image) {
     sh "docker build --pull=true -t ${image} ."
     sh "docker run -td --restart unless-stopped -tmpfs /tmp:exec --tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name ${image}"    
-    sh "docker cp ./ $(DOCKER_IMG):/opt/task"
+    sh "docker cp ./ ${image}:/opt/task"
 }
     
 													
